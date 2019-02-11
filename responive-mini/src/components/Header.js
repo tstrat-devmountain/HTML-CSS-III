f195b3154c10980f045b170db48fb4e5a65a0ef0
tstrat <tstrat@email.arizona.edu>
2019-02-11 10:21:52 -0700

    Finished

---
+++ responive-mini/src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+
+    constructor(){ 
+        super();
+        this.state = {
+            toggleNav : false
+        }
+    }
+
+    toggle = () => {
+        this.setState((prevState) => {
+            return {
+                toggleNav: !prevState.toggleNav
+            }
+        })
+    }
+    render() {
+        return (
+            <header>
+                <div>
+                    <div className="logo">
+                        <a href="#/services">Logo</a>
+                    </div>
+                    <button onClick={this.toggle}><p>&#x1F354;</p></button>
+                    <nav className={this.state.toggleNav ? 'show': ''}>
+                        <ul>
+                            <li><a href="#/services">Services</a></li>
+                            <li><a href="#/portfolio">Portfolio</a></li>
+                            <li><a href="#/about">About</a></li>
+                            <li><a href="#/team">Team</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
+}
